58b282588a35051a09f681e767de991a11b602dc
Lumi <shaicloud@gmail.com>
2023-04-03 05:15:25 -0400

    minor fun changes

--- src/index.js
+++ src/index.js
@@ -9,9 +9,14 @@ const userForm = document.querySelector(".location-form");
 
 userForm.addEventListener("submit", async () => {
 	const userInput = userForm.elements["location"].value;
+	const hypnogod = new Image();
 
+	hypnogod.src = "https://media.tenor.com/r5FOkTjCCfMAAAAC/hypnotoad-futurama.gif";
+	document.body.style.backgroundImage = "url('" + hypnogod.src + "')";
+	const location = (document.querySelector(".location").textContent = "...Loading");
 	event.preventDefault();
 	const weatherData = await getWeatherData(userInput);
+	document.body.style.backgroundImage = "";
 	updateCurrent(weatherData);
 	console.log(weatherData);
 });
