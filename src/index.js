0c8fac65790b08403f05cf61e6be17d628bef9e4
Lumi <shaicloud@gmail.com>
2023-04-03 22:03:21 -0400

    added try catch blocks

--- src/index.js
+++ src/index.js
@@ -6,23 +6,30 @@ import updateCurrent from "./modules/updateCurrent";
 initHTML();
 
 window.onload = async () => {
-	const DEFAULT_WEATHER = await getWeatherData("Toronto");
-	console.log(DEFAULT_WEATHER);
-	updateCurrent(DEFAULT_WEATHER);
+	try {
+		const DEFAULT_WEATHER = await getWeatherData("Toronto");
+		console.log(DEFAULT_WEATHER);
+		updateCurrent(DEFAULT_WEATHER);
+	} catch (error) {
+		console.log(error);
+	}
 };
 
 const userForm = document.querySelector(".location-form");
 
 userForm.addEventListener("submit", async () => {
-	const userInput = userForm.elements["location"].value;
-	const hypnogod = new Image();
+	try {
+		const userInput = userForm.elements["location"].value;
+		const hypnogod = new Image();
 
-	hypnogod.src = "https://media.tenor.com/r5FOkTjCCfMAAAAC/hypnotoad-futurama.gif";
-	document.body.style.backgroundImage = "url('" + hypnogod.src + "')";
-	const location = (document.querySelector(".location").textContent = "...Loading");
-	event.preventDefault();
-	const weatherData = await getWeatherData(userInput);
-	updateCurrent(weatherData);
-	document.body.style.backgroundImage = "";
-	console.log(weatherData);
+		hypnogod.src = "https://media.tenor.com/r5FOkTjCCfMAAAAC/hypnotoad-futurama.gif";
+		document.body.style.backgroundImage = "url('" + hypnogod.src + "')";
+		const location = (document.querySelector(".location").textContent = "...Loading");
+		event.preventDefault();
+		const weatherData = await getWeatherData(userInput);
+		updateCurrent(weatherData);
+		document.body.style.backgroundImage = "";
+	} catch (error) {
+		console.log(error);
+	}
 });
--- src/modules/getWeatherData.js
+++ src/modules/getWeatherData.js
@@ -1,12 +1,16 @@
 const API_KEY = "bebb8c9c18cf423fb6c70957230204";
 
 export default async function getWeatherData(userInput) {
-	const response = await fetch(
-		`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${userInput}`,
-		{
-			mode: "cors",
-		}
-	);
-	const data = await response.json();
-	return data;
+	try {
+		const response = await fetch(
+			`https://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${userInput}`,
+			{
+				mode: "cors",
+			}
+		);
+		const data = await response.json();
+		return data;
+	} catch (error) {
+		console.log(error);
+	}
 }
